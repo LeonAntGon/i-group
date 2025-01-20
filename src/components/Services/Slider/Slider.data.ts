9e023ea2a6d4b43a5a8ad98321047aaafac180a1
unknown <gonzalezleonardoantonio056@gmail.com>
2025-01-20 13:07:44 -0300

    adding img's sliders

--- src/components/Services/Slider/Slider.data.ts
+++ src/components/Services/Slider/Slider.data.ts
@@ -1,34 +1,34 @@
 export const sliderDataImages = [
   {
     id: 1,
-    urlImage: "house1.PNG",
+    urlImage: "house1.png",
   },
   {
     id: 2,
-    urlImage: "house2.PNG",
+    urlImage: "house2.png",
   },
   {
     id: 3,
-    urlImage: "house3.PNG",
+    urlImage: "house3.png",
   },
   {
     id: 4,
-    urlImage: "house4.PNG",
+    urlImage: "house4.png",
   },
   {
     id: 5,
-    urlImage: "house5.PNG",
+    urlImage: "house5.png",
   },
   {
     id: 6,
-    urlImage: "house6.PNG",
+    urlImage: "house6.png",
   },
   {
     id: 7,
-    urlImage: "house7.PNG",
+    urlImage: "house7.png",
   },
   {
     id: 8,
-    urlImage: "house8.PNG",
+    urlImage: "house8.png",
   }
 ];
